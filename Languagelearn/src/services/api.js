1838bc523f75fcf6064cc2eb1679b41a562eda9a
Vipin karthik M B <vipinkarthik.mb2023ece@sece.ac.in>
2025-08-31 15:28:10 +0530

    Updated with backend Deploy

--- Languagelearn/src/services/api.js
+++ Languagelearn/src/services/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://languagelearnbackend.onrender.com/api';
 
 export const signUp = async (formData) => {
   try {
     const response = await axios.post(`${API_URL}/signup`, formData);
     return response.data;
   } catch (error) {
-    return { success: false, error: error.response?.data?.message || 'Signup failed' };
+    return { 
+      success: false, 
+      error: error.response?.data?.message || 'Signup failed' 
+    };
   }
 };
